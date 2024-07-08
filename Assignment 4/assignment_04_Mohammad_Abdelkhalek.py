1adda058573f2e03f8698b000f76961425b402cf
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-07-08 20:52:04 +0300

    1. isPalindrome() <Added>

--- Assignment 4/assignment_04_Mohammad_Abdelkhalek.py
+++ Assignment 4/assignment_04_Mohammad_Abdelkhalek.py
@@ -3,3 +3,26 @@
 #####################################
 
 # Stacks and Queues
+
+# Using Stack
+def isPalindrome(string):
+    
+    string = ''.join(string.split()).lower()
+    
+    stack = []
+    
+    for i in string:
+        stack.append(i)
+        
+    reversed = ''
+    
+    for i in range(len(stack)):
+        reversed += stack.pop()
+        
+    if string == reversed:
+        return True
+    else:
+        return False
+    
+# print(isPalindrome("Never odd or even"))
+    
