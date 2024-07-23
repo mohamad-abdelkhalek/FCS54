1d58df00a377b37c56cfd177e8b795ccb2542b39
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-07-23 19:52:39 +0300

    Added class Node

---
+++ Assignment 5/assignment_05_Mohammad_Abdelkhalek.py
@@ -0,0 +1,7 @@
+class Node:
+    
+    def __init__(self, value, parent=None, right=None, left=None):
+        self.parent = parent
+        self.right = right
+        self.left = left
+        self.value = value
