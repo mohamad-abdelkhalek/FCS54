835cc13ea8952a788eebb582b52197409398857d
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-07-24 20:25:13 +0300

    Added printEquation function

--- Assignment 5/expressionTree.py
+++ Assignment 5/expressionTree.py
@@ -3,3 +3,25 @@ class Node:
         self.value = value
         self.left = None
         self.right = None
+
+def printEquation(root):
+    # Helper function for in-order traversal
+    def inorder(node):
+        if node is None:
+            return ""
+        
+        if node.left is None and node.right is None:
+            return str(node.value)
+        
+        left_expr = inorder(node.left)
+        right_expr = inorder(node.right)
+        
+        return "(" + left_expr + str(node.value) + right_expr + ")"
+    
+    equation = inorder(root)
+    
+    # Removing the outermost parentheses
+    if equation[0] == '(' and equation[-1] == ')':
+        equation = equation[1:-1]
+    
+    return equation
